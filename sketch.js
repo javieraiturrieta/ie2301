5438ff24ef7e77fc3fdb993b1829d6e46d41e542
javieraiturrieta <142560860+javieraiturrieta@users.noreply.github.com>
2023-09-21 23:07:59 -0300

    commit inicial

--- sketch.js
+++ sketch.js
@@ -5,65 +5,121 @@ function setup() {
 function draw() {
   background(220);
 }
-let circles = [];
-let numCircles = 10;
+let flakes = [];
+let trail = [];
 
 function setup() {
-  createCanvas(800, 600);
-  noStroke();
-
-  for (let i = 0; i < numCircles; i++) {
-    circles.push(new Circle());
-  }
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(255);
+  background(0);
 
-  for (let circle of circles) {
-    circle.move();
-    circle.display();
+  // Dibuja la estela de copos de nieve anteriores
+  for (let i = 0; i < trail.length; i++) {
+    let t = trail[i];
+    t.update();
+    t.display();
+    if (t.opacity <= 0) {
+      trail.splice(i, 1);
+    }
+  }
+
+  // Dibuja los copos de nieve actuales
+  for (let i = flakes.length - 1; i >= 0; i--) {
+    flakes[i].update();
+    flakes[i].display();
+
+    // Agrega una estela detrás de cada copo de nieve
+    let trailFlake = flakes[i].createTrailFlake();
+    trail.push(trailFlake);
+
+    if (flakes[i].size <= 0) {
+      flakes.splice(i, 1);
+    }
   }
 }
 
-class Circle {
-  constructor() {
-    this.x = random(width);
-    this.y = random(height);
-    this.radius = random(20, 50);
-    this.xSpeed = random(-2, 2);
-    this.ySpeed = random(-2, 2);
-    this.color = color(random(150, 200), 0, random(150, 255), 100);
-    this.history = [];
+function mouseClicked() {
+  let x = mouseX;
+  let y = mouseY;
+  let numFlakes = int(random(5, 20)); // Número de copos en cada clic
+
+  for (let i = 0; i < numFlakes; i++) {
+    let size = random(10, 30);
+    let flakeColor = color(random(255), random(255), random(255));
+    let angle = random(TWO_PI); // Dirección de inicio aleatoria
+    flakes.push(new Snowflake(x, y, size, flakeColor, angle));
+  }
+}
+
+class Snowflake {
+  constructor(x, y, size, flakeColor, angle) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.flakeColor = flakeColor;
+    this.speed = random(0.5, 1.5); // Velocidad más lenta
+    this.angle = angle; // Dirección de movimiento
   }
 
-  move() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
+  update() {
+    // Calcula las nuevas coordenadas en función del ángulo y la velocidad
+    this.x += cos(this.angle) * this.speed;
+    this.y += sin(this.angle) * this.speed;
 
-    if (this.x <= 0 || this.x >= width) {
-      this.xSpeed *= -1;
-    }
-    if (this.y <= 0 || this.y >= height) {
-      this.ySpeed *= -1;
+    // Cambia la dirección al llegar a los bordes del lienzo
+    if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
+      this.angle += PI; // Cambia la dirección en 180 grados
     }
 
-    this.history.push(createVector(this.x, this.y));
-    if (this.history.length > 20) {
-      this.history.shift();
-    }
+    this.size -= 0.1;
   }
 
   display() {
-    fill(this.color);
-
-    for (let i = 0; i < this.history.length; i++) {
-      let pos = this.history[i];
-      let diameter = map(i, 0, this.history.length, this.radius * 2, 0);
-      ellipse(pos.x, pos.y, diameter);
-    }
-
-    this.x = mouseX;
-    this.y = mouseY;
+    noStroke();
+    fill(this.flakeColor);
+    drawSnowflake(this.x, this.y, this.size);
   }
+
+  createTrailFlake() {
+    return new TrailFlake(this.x, this.y, this.size, this.flakeColor);
+  }
+}
+
+class TrailFlake {
+  constructor(x, y, size, flakeColor) {
+    this.x = x;
+    this.y = y;
+    this.size = size * 0.5; // Tamaño reducido
+    this.flakeColor = color(red(flakeColor), green(flakeColor), blue(flakeColor), 50); // Opacidad más reducida
+    this.opacity = 50;
+  }
+
+  update() {
+    this.opacity -= 1; // Disminuir la opacidad gradualmente
+  }
+
+  display() {
+    noStroke();
+    fill(this.flakeColor);
+    drawSnowflake(this.x, this.y, this.size);
+  }
+}
+
+function drawSnowflake(x, y, s) {
+  let angle = TWO_PI / 6;
+  let halfSize = s / 2;
+
+  beginShape();
+  for (let i = 0; i < 6; i++) {
+    let xOffset = cos(angle * i) * halfSize;
+    let yOffset = sin(angle * i) * halfSize;
+    vertex(x + xOffset, y + yOffset);
+
+    let nextX = cos(angle * (i + 1)) * halfSize;
+    let nextY = sin(angle * (i + 1)) * halfSize;
+    line(x + xOffset, y + yOffset, x + nextX, y + nextY);
+  }
+  endShape(CLOSE);
 }
